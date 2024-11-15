689c69435c60085b9b38afdd9bd90c8c1fca660f
CristobalRRR <crisitoramiz@hotmail.com>
2024-11-15 11:15:23 -0300

    Co-authored-by: Alfredo Becerra <AlfredoBE@users.noreply.github.com>

--- pokedexApp/screens/CapturedScreen.js
+++ pokedexApp/screens/CapturedScreen.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import {
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+  } from "react-native";
+import { fetchMoveData } from "../data/MoveData";
 import { fetchPokemonData } from '../data/PokemonData';
+import { URL_FIREBASE } from "@env";
 
 export default function CapturedScreen() {
   const capturedPokemons = PokemonData.filter(pokemon => pokemon.captured);
+
+  const fetchFavorites = async () => {
+    try {
+      const response = await axios.get(`${URL}/favorites.json`);
+      const favoritesData = response.data;
+      console.log("Datos de favoritos:", favoritesData);
+      return favoritesData;
+    } catch (error) {
+      console.error("Error al obtener los favoritos:", error);
+      return null;
+    }
+  };
 
   return (
     <FlatList
